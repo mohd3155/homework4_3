27b058665a6c158b6d4568c68490818dcd9b443b
Mohammed <moh9_kamal@hotmail.com>
2022-12-26 22:08:00 +0200

    Initial commit

---
+++ home work4-3.py
@@ -0,0 +1,12 @@
+
+
+a = float(input("enter first number"))
+b = float(input("enter second number"))
+c = float(input("enter third number"))
+d = float(input("enter forth number"))
+e = float(input("enter fifth number"))
+
+nums=[a,b,c,d,e]
+print(nums)
+nums.sort()
+print(nums[0],nums[4])
